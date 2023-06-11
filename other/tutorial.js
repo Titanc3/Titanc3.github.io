ba7d4bbcf3d8c7e4b0dcc4340ca46b2cfa69d82f
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2023-06-11 10:43:16 -0400

    Create tutorial.js

---
+++ other/tutorial.js
@@ -0,0 +1,27 @@
+function ReadCookie(cookieKey) { 
+   var allcookies = document.cookie.split("; ") 
+   // split str into list str="key=value; key=value; ..." list="key=value", "key=value", ... 
+ 
+   for (let i = 0; i < allcookies.length; i++) { 
+      let pair = allcookies[i].split("="); 
+      if (pair[0] == cookieKey) { 
+        return pair[1]; //pair[0] = key | pair[1] = value 
+      } 
+   } 
+  return null 
+} 
+function tutorialCookie(message) { 
+  let d = new Date;
+  
+  if (ReadCookie("tutorial") == "finished") { document.cookie = `tutorial=finished; expires=Thu, 26 Jan ${d.getFullYear()+1} 12:00:00 UTC; path=/`; } 
+   
+  else if ("https://titanc3.github.io"+ReadCookie("tutorial") == window.location.href) { 
+    alert(`${message}`); 
+    document.getElementById("tutorialNextLink").style.border = "2px solid yellow"; 
+    document.cookie = `tutorial=/other/time_period/period; expires=Thu, 26 Jan ${d.getFullYear()+1} 12:00:00 UTC; path=/`; 
+  } 
+  else {
+    if (confirm("Can you finish the tutorial before browsing around?") == true) { window.location.href="https://titanc3.github.io"+ReadCookie("tutorial"); }
+    else { document.cookie = `tutorial=finished; expires=Thu, 26 Jan ${d.getFullYear()+1} 12:00:00 UTC; path=/`; }
+  }
+};
